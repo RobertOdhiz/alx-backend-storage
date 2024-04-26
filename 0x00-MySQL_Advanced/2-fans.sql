c4e5396cc78bd8ae47e0e5bc3d724f23134419ff
Robert Odhiambo <odhizrober254@gmail.com>
2024-04-26 13:16:42 +0300

    glam rock

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,3 +1,8 @@
+-- Write SQL script that ranks country origins of bands
+-- Ordered by the number of (non-unique) fans
+-- Column must be origin and nb_fans
+-- Script can be executed on any database
+
 SELECT origin, SUM(nb_fans) AS total_fans
 FROM metal_bands
 GROUP BY origin
